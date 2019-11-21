00f98b8a15199abe71d3d2cda9437bbc20f66973
Leisvan Cordero <rathmA@LIVE.COM>
2019-11-21 16:08:10 -0500

    MainPageConcept added

--- Yugen.Mosaic.Uwp/MainPageConcept.xaml.cs
+++ Yugen.Mosaic.Uwp/MainPageConcept.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,6 +29,39 @@ namespace Yugen.Mosaic.Uwp
         public MainPageConcept()
         {
             this.InitializeComponent();
+            ExtendToTitleBar();
         }
+
+        private void ExtendToTitleBar()
+        {
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            //Active:
+            titleBar.BackgroundColor = Colors.Transparent;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+
+            //Inactive:
+            titleBar.InactiveBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            AddMasterUI.Visibility = Visibility.Visible;
+        }
+
+        private void Grid_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            AddMasterUI.Visibility = (ViewModel.MasterBpmSource != null) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ViewModel.AddMasterButton_Click(sender, e);
+            AddMasterUI.Visibility = Visibility.Collapsed;
+        }
+
     }
 }
