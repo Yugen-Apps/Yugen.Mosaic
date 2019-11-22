7868fb8c254e8dbba08b2462939c53fa499d7b65
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2019-11-22 09:21:19 +0400

    moved mainpageconcept code to mainpage

--- Yugen.Mosaic.Uwp/MainPage.xaml.cs
+++ Yugen.Mosaic.Uwp/MainPage.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Yugen.Mosaic.Uwp
 {
@@ -10,10 +15,42 @@ namespace Yugen.Mosaic.Uwp
     public sealed partial class MainPage : Page
     {
         public MainViewModel ViewModel { get; set; } = new MainViewModel();
-
         public MainPage()
         {
             this.InitializeComponent();
+            ExtendToTitleBar();
         }
+
+        private void ExtendToTitleBar()
+        {
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            //Active:
+            titleBar.BackgroundColor = Colors.Transparent;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+
+            //Inactive:
+            titleBar.InactiveBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            AddMasterUI.Visibility = Visibility.Visible;
+        }
+
+        private void Grid_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            AddMasterUI.Visibility = (ViewModel.MasterBpmSource != null) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ViewModel.AddMasterButton_Click(sender, e);
+            AddMasterUI.Visibility = Visibility.Collapsed;
+        }
+
     }
 }
